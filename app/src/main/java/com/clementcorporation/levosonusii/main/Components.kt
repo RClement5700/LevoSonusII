3d212f1b3b0c7478b0449ee50f21130d1fdce1e4
rclement5700 <rclement5700>
2022-05-24 15:21:37 -0700

    Add notifications button and menu button to HomeScreen topBar

--- app/src/main/java/com/clementcorporation/levosonusii/main/Components.kt
+++ app/src/main/java/com/clementcorporation/levosonusii/main/Components.kt
@@ -2,6 +2,7 @@ package com.clementcorporation.levosonusii.main
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -9,24 +10,28 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.clementcorporation.levosonusii.R
 import com.clementcorporation.levosonusii.main.Constants.CURVATURE
-import com.clementcorporation.levosonusii.main.Constants.ELEVATION
 import com.clementcorporation.levosonusii.main.Constants.PADDING
 
 private const val LOGO_DESCRIPTION = "Levo Sonus Logo"
@@ -56,30 +61,53 @@ fun LevoSonusLogo(size: Dp = 96.dp, showText: Boolean = true) {
 }
 
 @Composable
-fun LSAppBar(employeeName: String, onClick: () -> Unit = {}) {
+fun LSAppBar(employeeName: String, onClickProfilePic: () -> Unit = {},
+             onClickAlertBtn: () -> Unit = {}, onClickMenuBtn: () -> Unit = {}
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(PADDING.dp)
-            .background(shape = RoundedCornerShape(CURVATURE.dp), color = Color.White)
-            .clickable {
-                onClick
-            },
+            .background(shape = RoundedCornerShape(CURVATURE.dp), color = Color.White),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End
+        horizontalArrangement = Arrangement.Start
     ) {
-        LSProfileIcon(modifier = Modifier.background(color = Color.LightGray, shape = CircleShape))
+        LSProfileIcon(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.LightGray, CircleShape)
+                .clickable {
+                    onClickProfilePic
+                }
+        )
         Text(
             modifier = Modifier.padding(PADDING.dp),
             text = employeeName,
             color = Color.Gray,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            fontSize = 12.sp
         )
+        Spacer(Modifier.weight(1f))
+        IconButton(
+            onClick = {
+                onClickAlertBtn
+            }
+        ) {
+            Icon(imageVector = Icons.Default.Notifications, contentDescription = "Menu Button")
+        }
+        IconButton(
+            onClick = {
+                onClickMenuBtn
+            }
+        ) {
+            Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Menu Button")
+        }
     }
 }
 
 @Composable
-fun LSProfileIcon(modifier: Modifier = Modifier) {
+fun LSProfileIcon(modifier: Modifier) {
     Image(
         modifier = modifier,
         painter = painterResource(id = R.drawable.levosonus_rocket_logo), //replace with user uploaded profile pic
